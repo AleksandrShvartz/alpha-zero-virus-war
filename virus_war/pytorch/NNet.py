0575f6d6eb4996cc8af8118fc7e58e88dcd63a26
lyphrowny <strukov.as@edu.spbstu.ru>
2023-03-27 21:53:51 +0300

    Add keras nnet

--- virus_war/pytorch/NNet.py
+++ virus_war/pytorch/NNet.py
@@ -17,7 +17,7 @@ from .VirusWarNNet import VirusWarNNet as vwnnet
 args = dotdict({
     'lr': 0.001,
     'dropout': 0.3,
-    'epochs': 10,
+    'epochs': 12,
     'batch_size': 64,
     'cuda': torch.cuda.is_available(),
     'num_channels': 512,
